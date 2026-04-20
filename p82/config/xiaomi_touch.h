6957f6b646d1c919e175e6f9000eb50c8635273c
niyongqi <niyongqi@xiaomi.com>
2026-04-20 20:43:37 +0800

    Kernel: Xiaomi touch driver changes for Xiaomi Pad 8 Pro Android W
    The Patch based on QualComm release TAG: LA.VENDOR.15.4.0.r1-15000-r1.0.r1_00044.1
    
    Signed-off-by: niyongqi <niyongqi@xiaomi.com>

---
+++ p82/config/xiaomi_touch.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+
+#define CONFIG_TOUCHSCREEN_NT36xxx_HOSTDL_SPI 1
+#define CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
+#define CONFIG_TOUCHSCREEN_NVT_DEBUG_FS
+#define CONFIG_TOUCH_STYLUS_SUPPORT
+
+// #define CONFIG_TOUCH_BOOST //func not ready
